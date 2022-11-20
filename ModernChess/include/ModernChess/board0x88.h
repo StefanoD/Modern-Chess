7e81ce69375be598995e82c4745c01e600c37c53
Stefano Di Martino <stefano.d@posteo.de>
2022-11-20 20:16:48 +0100

    Add 0x88_math.h and board0x88.cpp

---
+++ ModernChess/include/ModernChess/board0x88.h
@@ -0,0 +1,26 @@
+
+#include "stdafx.h"
+#include "0x88_math.h"
+#include "transposition.h"
+
+sboard b;
+
+
+void clearBoard();
+
+/******************************************************************************
+* fillSq() and clearSq(), beside placing a piece on a given square or erasing *
+* it,  must  take care for all the incrementally updated  stuff:  hash  keys, *
+* piece counters, material and pcsq values, pawn-related data, king location. *
+******************************************************************************/
+
+void fillSq(uint8_t color, uint8_t piece, int8_t sq);
+
+
+void clearSq(int8_t sq);
+
+
+int board_loadFromFen(char * fen);
+
+
+void board_display();
