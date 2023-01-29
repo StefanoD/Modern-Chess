572192cd4895e1c68cf0a21605b950ad1533569b
Stefano Di Martino <stefano.d@posteo.de>
2023-01-29 18:03:49 +0100

    - Move comment

--- ModernChess/include/ModernChess/Board.h
+++ ModernChess/include/ModernChess/Board.h
@@ -12,9 +12,6 @@ namespace ModernChess
     // TODO function to map from geometric to bitmap
     // TODO function to bitwise and all bitmaps -> Piece captures (different colors), illegal moves (same colors) etc.
     // TODO function to map FEN notation to board state
-    /**
-     * @brief Every piece-type and every color has its own board representation
-     */
     class Board
     {
     public:
@@ -25,6 +22,7 @@ namespace ModernChess
         [[nodiscard]] std::string toString() const;
 
     protected:
+        // Every piece-type and every color has its own board representation
         BitBoard m_blackRookBitBoard;
         BitBoard m_blackKnightBitBoard;
         BitBoard m_blackBishopBitBoard;
