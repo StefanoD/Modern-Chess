c04f4b68698af00016070dbedfdf3beebae39a1e
Stefano Di Martino <stefano.d@posteo.de>
2023-01-29 13:27:26 +0100

    Init Board

--- ModernChess/include/ModernChess/Board.h
+++ ModernChess/include/ModernChess/Board.h
@@ -1,13 +1,38 @@
 #pragma once
 
+#include <bitset>
+#include <bit>
+
 namespace ModernChess
 {
+    // TODO function to set starting state
+    // TODO function to map from geometric to bitmap
+    // TODO function to bitwise and all bitmaps -> board state
+    // TODO function to map FEN notation to board state
+    /**
+     * @brief Every piece-type and every color has its own board representation
+     */
+    class Board
+    {
+    public:
 
-class Board
-{
-public:
+        Board();
 
-};
+    private:
+        std::bitset<64> m_blackRookBitBoard;
+        std::bitset<64> m_blackKnightBitBoard;
+        std::bitset<64> m_blackBishopBitBoard;
+        std::bitset<64> m_blackQueenBitBoard;
+        std::bitset<64> m_blackKingBitBoard;
+        std::bitset<64> m_blackPawnBitBoard;
+
+        std::bitset<64> m_whiteRookBitBoard;
+        std::bitset<64> m_whiteKnightBitBoard;
+        std::bitset<64> m_whiteBishopBitBoard;
+        std::bitset<64> m_whiteQueenBitBoard;
+        std::bitset<64> m_whiteKingBitBoard;
+        std::bitset<64> m_whitePawnBitBoard;
+    };
 
 
 }
--- ModernChess/src/Board.cpp
+++ ModernChess/src/Board.cpp
@@ -1,5 +1,33 @@
-//
-// Created by Stefano Di Martino on 29.01.23.
-//
-
 #include "ModernChess/Board.h"
+
+namespace ModernChess
+{
+
+    Board::Board()
+    {
+        m_blackRookBitBoard.set(0, true);
+        m_blackRookBitBoard.set(7, true);
+
+        m_blackKnightBitBoard.set(1, true);
+        m_blackKnightBitBoard.set(6, true);
+
+        m_blackBishopBitBoard.set(2, true);
+        m_blackBishopBitBoard.set(5, true);
+
+        m_blackQueenBitBoard.set(3, true);
+
+        m_blackKingBitBoard.set(4, true);
+
+        for (int i = 8; i < 16; ++i)
+        {
+            m_blackPawnBitBoard.set(i, true);
+        }
+
+        m_whiteRookBitBoard = std::rotr(m_blackRookBitBoard.to_ulong(), 8);
+        m_whiteKnightBitBoard = std::rotr(m_blackKnightBitBoard.to_ulong(), 8);
+        m_whiteBishopBitBoard = std::rotr(m_blackBishopBitBoard.to_ulong(), 8);
+        m_whiteQueenBitBoard = std::rotr(m_blackQueenBitBoard.to_ulong(), 8);
+        m_whiteKingBitBoard = std::rotr(m_blackKingBitBoard.to_ulong(), 8);
+        m_whitePawnBitBoard = std::rotr(m_blackPawnBitBoard.to_ulong(), 24);
+    }
+}
