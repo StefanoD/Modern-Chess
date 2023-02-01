f41a77174503de05f1530a7cb218d1c7cbc1751c
Stefano Di Martino <stefano.d@posteo.de>
2023-02-01 23:27:57 +0100

    - add namespace BitBoardConstants

--- ModernChess/include/ModernChess/BitBoardConstants.h
+++ ModernChess/include/ModernChess/BitBoardConstants.h
@@ -3,7 +3,7 @@
 #include <cinttypes>
 
 namespace ModernChess {
-    // Constants from https://www.chessprogramming.org/Square_Mapping_Considerations
+    // Constants from https://www.chessprogramming.org/Square_Mapping_Considerations#LittleEndianRankFileMapping
     // We use Little-Endian Rank-File Mapping
 
     using BitBoardState = uint64_t;
@@ -20,14 +20,16 @@ namespace ModernChess {
         SouthEast = -7,
     };
 
-    constexpr BitBoardState aFile = 0x0101010101010101;
-    constexpr BitBoardState hFile = 0x8080808080808080;
-    constexpr BitBoardState firstRank = 0x00000000000000FF;
-    constexpr BitBoardState eightsRank = 0xFF00000000000000;
-    constexpr BitBoardState a1H8Diagonal = 0x8040201008040201;
-    constexpr BitBoardState h1A8Antidiagonal = 0x0102040810204080;
-    constexpr BitBoardState lightSquares = 0x55AA55AA55AA55AA;
-    constexpr BitBoardState darkSquares = 0xAA55AA55AA55AA55;
-    constexpr BitBoardState notAFile = ~aFile;
-    constexpr BitBoardState notHFile = ~hFile;
+    namespace BitBoardConstants {
+        constexpr BitBoardState aFile = 0x0101010101010101;
+        constexpr BitBoardState hFile = 0x8080808080808080;
+        constexpr BitBoardState firstRank = 0x00000000000000FF;
+        constexpr BitBoardState eightsRank = 0xFF00000000000000;
+        constexpr BitBoardState a1H8Diagonal = 0x8040201008040201;
+        constexpr BitBoardState h1A8Antidiagonal = 0x0102040810204080;
+        constexpr BitBoardState lightSquares = 0x55AA55AA55AA55AA;
+        constexpr BitBoardState darkSquares = 0xAA55AA55AA55AA55;
+        constexpr BitBoardState notAFile = ~aFile;
+        constexpr BitBoardState notHFile = ~hFile;
+    }
 }
