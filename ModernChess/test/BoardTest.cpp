a39acdc86cdf0023f69a1f967fff0303f4af4315
Stefano Di Martino <stefano.d@posteo.de>
2023-02-03 23:33:19 +0100

    - Add test AFile

--- ModernChess/test/BoardTest.cpp
+++ ModernChess/test/BoardTest.cpp
@@ -44,14 +44,13 @@ namespace
 
     TEST(Board, notHFile)
     {
-        BitBoardState board = BoardState::allSquaresOccupied;
-        board &= BitBoardConstants::notHFile;
+        const BitBoardState board = BoardState::allSquaresOccupied & BitBoardConstants::notHFile;
 
-        std::vector<Square> hFile {
+        const std::vector<Square> hFile {
             h1, h2, h3, h4, h5, h6, h7, h8
         };
 
-        std::vector<Square> notHFile {
+        const std::vector<Square> notHFile {
             a1, b1, c1, d1, e1, f1, g1,
             a2, b2, c2, d2, e2, f2, g2,
             a3, b3, c3, d3, e3, f3, g3,
@@ -70,6 +69,38 @@ namespace
         for (Square square : notHFile)
         {
             EXPECT_TRUE(BitboardOperations::isOccupied(board, square));
+        }
+
+        std::cout << BitBoard::printBitBoard(board) << std::endl;
+    }
+
+    TEST(Board, AFile)
+    {
+        const BitBoardState board = BoardState::allSquaresOccupied & BitBoardConstants::aFile;
+
+        const std::vector<Square> aFile {
+            a1, a2, a3, a4, a5, a6, a7, a8,
+        };
+
+        const std::vector<Square> notAFile {
+            b1, c1, d1, e1, f1, g1, h1,
+            b2, c2, d2, e2, f2, g2, h2,
+            b3, c3, d3, e3, f3, g3, h3,
+            b4, c4, d4, e4, f4, g4, h4,
+            b5, c5, d5, e5, f5, g5, h5,
+            b6, c6, d6, e6, f6, g6, h6,
+            b7, c7, d7, e7, f7, g7, h7,
+            b8, c8, d8, e8, f8, g8, h8
+        };
+
+        for (Square square : aFile)
+        {
+            EXPECT_TRUE(BitboardOperations::isOccupied(board, square));
+        }
+
+        for (Square square : notAFile)
+        {
+            EXPECT_FALSE(BitboardOperations::isOccupied(board, square));
         }
 
         std::cout << BitBoard::printBitBoard(board) << std::endl;
