86f738e29e72b4e8f14fa24fd2121c8dbe5d59f7
Stefano Di Martino <stefano.d@posteo.de>
2023-02-03 23:28:17 +0100

    - Add test OneStepEast

--- ModernChess/test/BoardTest.cpp
+++ ModernChess/test/BoardTest.cpp
@@ -143,6 +143,44 @@ namespace
         std::cout << board.printBitBoard(bitBoardState) << std::endl;
     }
 
+    TEST(Board, OneStepEast)
+    {
+        BitBoard board; // Has initial board state with correct positions
+
+        // All figures have been moved one step north
+        const BitBoardState bitBoardState = BitboardOperations::oneStepEast(board.getOccupiedSquares());
+
+        // Test if squares are occupied
+        for (Square square = Square::b1; square <= Square::h1; ++square)
+        {
+            EXPECT_TRUE(BitboardOperations::isOccupied(bitBoardState, Square(square)));
+        }
+
+        // Test for occupation of white figures
+        for (Square square = Square::b2; square <= Square::h2; ++square)
+        {
+            EXPECT_TRUE(BitboardOperations::isOccupied(bitBoardState, Square(square)));
+        }
+
+        // Test for occupation of black figures
+        for (Square square = Square::b7; square <= Square::h7; ++square)
+        {
+            EXPECT_TRUE(BitboardOperations::isOccupied(bitBoardState, Square(square)));
+        }
+
+        // Test for occupation of black figures
+        for (Square square = Square::b8; square <= Square::h8; ++square)
+        {
+            EXPECT_TRUE(BitboardOperations::isOccupied(bitBoardState, Square(square)));
+        }
+
+        const BitBoardState aFile = bitBoardState & BitBoardConstants::aFile;
+        // Because the pieces moved to east, we expect the complete a-file to be empty!
+        EXPECT_EQ(aFile, BoardState::empty);
+
+        std::cout << board.printBitBoard(bitBoardState) << std::endl;
+    }
+
     TEST(Board, SquaresAreUniquelyOccupied)
     {
         ExtendedBoard board;
