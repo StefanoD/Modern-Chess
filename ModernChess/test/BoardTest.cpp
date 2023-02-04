3d20d39b7dc80325b68c13910dad400b62c521f9
Stefano Di Martino <stefano.d@posteo.de>
2023-02-04 14:02:54 +0100

    - Add test EightsRank

--- ModernChess/test/BoardTest.cpp
+++ ModernChess/test/BoardTest.cpp
@@ -108,6 +108,37 @@ namespace
         print(std::cout, board) << std::endl;
     }
 
+    TEST(Board, EightsRank)
+    {
+        const BitBoardState board = BoardState::allSquaresOccupied & BitBoardConstants::eightsRank;
+
+        const std::vector<Square> eightsRank {
+                a8, b8, c8, d8, e8, f8, g8, h8
+        };
+
+        const std::vector<Square> notEightsRank {
+                a1, b1, c1, d1, e1, f1, g1, h1,
+                a2, b2, c2, d2, e2, f2, g2, h2,
+                a3, b3, c3, d3, e3, f3, g3, h3,
+                a4, b4, c4, d4, e4, f4, g4, h4,
+                a5, b5, c5, d5, e5, f5, g5, h5,
+                a6, b6, c6, d6, e6, f6, g6, h6,
+                a7, b7, c7, d7, e7, f7, g7, h7,
+        };
+
+        for (Square square : eightsRank)
+        {
+            EXPECT_TRUE(BitboardOperations::isOccupied(board, square));
+        }
+
+        for (Square square : notEightsRank)
+        {
+            EXPECT_FALSE(BitboardOperations::isOccupied(board, square));
+        }
+
+        print(std::cout, board) << std::endl;
+    }
+
     TEST(Board, AFile)
     {
         const BitBoardState board = BoardState::allSquaresOccupied & BitBoardConstants::aFile;
