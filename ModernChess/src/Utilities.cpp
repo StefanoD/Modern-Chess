c0450840354ed56d1b66fc8dd6e76225d566f954
Stefano Di Martino <stefano.d@posteo.de>
2023-02-05 01:53:02 +0100

    - Better formatting for bit board state printing.

--- ModernChess/src/Utilities.cpp
+++ ModernChess/src/Utilities.cpp
@@ -8,15 +8,26 @@ std::ostream& operator<<(std::ostream& os, const ModernChess::BitBoardState bitB
     // The enumeration is actually mirrored. So correct this for printing.
     const BitBoardState mirroredBoard = BitBoardOperations::mirrorHorizontal(bitBoardState);
 
+    os << "8 | ";
+
     for (Square square = Square::h8; square >= Square::a1; --square)
     {
-        os << BitBoardOperations::isOccupied(mirroredBoard, Square(square));
+        os << BitBoardOperations::isOccupied(mirroredBoard, Square(square)) << " ";
 
         if (square % 8 == 0)
         {
             os << std::endl;
+            const int rank = square / 8;
+
+            if (rank != 0)
+            {
+                os << rank << " | ";
+            }
+
         }
     }
+    os << "-------------------" << std::endl;
+    os << "    a b c d e f g h" << std::endl;
 
     return os;
 }
