16bfa52b7e6743a98cb5b4e3b2cc17f8040d84c9
Stefano Di Martino <stefano.d@posteo.de>
2023-02-11 12:45:58 +0100

    - Add test RookWestAttacks

--- ModernChess/test/AttacksGenerationTest.cpp
+++ ModernChess/test/AttacksGenerationTest.cpp
@@ -151,7 +151,52 @@ namespace
         print(std::cout, rayAttackBoard) << std::endl;
     }
 
+    TEST(AttacksGenerationTest, RookWestAttacks)
+    {
+        // Occupy rooks on g1, h8, a7
+        BitBoardState whiteRooksBoard = BitBoardOperations::occupySquare(BoardState::empty, Square::g1);
+        whiteRooksBoard = BitBoardOperations::occupySquare(whiteRooksBoard, Square::h8);
+        // Edge case: rook is on a7 which is already the most west position
+        whiteRooksBoard = BitBoardOperations::occupySquare(whiteRooksBoard, Square::a7);
 
+        // Occupy figures on a1 and a8
+        BitBoardState playBoard = BitBoardOperations::occupySquare(BoardState::empty, Square::a8);
+        playBoard = BitBoardOperations::occupySquare(playBoard, Square::a1);
+
+        // Seem not to be necessary, but the attacking pieces are usually also part of the play board
+        playBoard |= whiteRooksBoard;
+
+        // We need to pass a rayAttackBoard with empty squares as second parameter where the bits are set to 1
+        const BitBoardState emptySquaresBoard = ~playBoard;
+        const BitBoardState rayAttackBoard = RookAttack::west(whiteRooksBoard, emptySquaresBoard);
+
+        const std::vector<Square> attackRay {
+                a1, b1, c1, d1, e1, f1,
+                a8, b8, c8, d8, e8, f8, g8
+        };
+
+        const std::vector<Square> notAttackedSquares {
+                                     g1, h1,
+                 b2, c2, d2, e2, f2, g2, h2,
+                 b3, c3, d3, e3, f3, g3,
+                 b4, c4, d4, e4, f4, g4,
+                 b5, c5, d5, e5, f5, g5,
+                 b6, c6, d6, e6, f6, g6,
+                 b7, c7, d7, e7, f7, g7, h7, h8
+        };
+
+        for (const Square square : attackRay)
+        {
+            EXPECT_TRUE(BitBoardOperations::isOccupied(rayAttackBoard, square));
+        }
+
+        for (const Square square : notAttackedSquares)
+        {
+            EXPECT_FALSE(BitBoardOperations::isOccupied(rayAttackBoard, square));
+        }
+
+        print(std::cout, rayAttackBoard) << std::endl;
+    }
 }
 
 
