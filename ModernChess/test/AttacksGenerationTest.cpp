6f63aab877278dbad1852774491717c197e053ea
Stefano Di Martino <stefano.d@posteo.de>
2023-02-11 13:52:15 +0100

    - Add test BishopSouthEastAttacks

--- ModernChess/test/AttacksGenerationTest.cpp
+++ ModernChess/test/AttacksGenerationTest.cpp
@@ -240,6 +240,49 @@ namespace
 
         print(std::cout, rayAttackBoard) << std::endl;
     }
+
+    TEST(AttacksGenerationTest, BishopSouthEastAttacks)
+    {
+        // Occupy bishop on b7
+        BitBoardState whiteRooksBoard = BitBoardOperations::occupySquare(BoardState::empty, Square::b7);
+
+        // Occupy figures on h1
+        BitBoardState playBoard = BitBoardOperations::occupySquare(BoardState::empty, Square::h1);
+
+        // Seem not to be necessary, but the attacking pieces are usually also part of the play board
+        playBoard |= whiteRooksBoard;
+
+        // We need to pass a rayAttackBoard with empty squares as second parameter where the bits are set to 1
+        const BitBoardState emptySquaresBoard = ~playBoard;
+        const BitBoardState rayAttackBoard = BishopAttack::southEast(whiteRooksBoard, emptySquaresBoard);
+
+        const std::vector<Square> attackRay {
+                c6, d5, e4, f3, g2, h1
+        };
+
+        const std::vector<Square> notAttackedSquares {
+                a1, b1, c1, d1, e1, f1, g1,
+                a2, b2, c2, d2, e2, f2,     h2,
+                a3, b3, c3, d3, e3,     g3, h3,
+                a4, b4, c4, d4,     f4, g4, h4,
+                a5, b5, c5,     e5, f5, g5, h5,
+                a6, b6,     d6, e6, f6, g6, h6,
+                a7, b7, c7, d7, e7, f7, g7, h7,
+                a8, b8, c8, d8, e8, f8, g8, h8
+        };
+
+        for (const Square square : attackRay)
+        {
+            EXPECT_TRUE(BitBoardOperations::isOccupied(rayAttackBoard, square));
+        }
+
+        for (const Square square : notAttackedSquares)
+        {
+            EXPECT_FALSE(BitBoardOperations::isOccupied(rayAttackBoard, square));
+        }
+
+        print(std::cout, rayAttackBoard) << std::endl;
+    }
 }
 
 
