ba32cd0f438550e12e29b5fd4dd914c845d45116
Stefano Di Martino <stefano.d@posteo.de>
2023-02-23 12:47:10 +0100

    - implement bit scan forward

--- ModernChess/include/ModernChess/BitBoardOperations.h
+++ ModernChess/include/ModernChess/BitBoardOperations.h
@@ -68,4 +68,31 @@ namespace ModernChess::BitBoardOperations
     {
         return mirrorHorizontal(flipVertical(board));
     }
+
+    constexpr int bitScanForwardLookup[64] =
+    {
+            0, 47,  1, 56, 48, 27,  2, 60,
+            57, 49, 41, 37, 28, 16,  3, 61,
+            54, 58, 35, 52, 50, 42, 21, 44,
+            38, 32, 29, 23, 17, 11,  4, 62,
+            46, 55, 26, 59, 40, 36, 15, 53,
+            34, 51, 20, 43, 31, 22, 10, 45,
+            25, 39, 14, 33, 19, 30,  9, 24,
+            13, 18,  8, 12,  7,  6,  5, 63
+    };
+
+    /**
+     * bitScanForward
+     * @author Kim Walisch (2012)
+     * @param bitBoard bitboard to scan
+     * @precondition bitBoard != 0
+     * @see https://www.chessprogramming.org/BitScan
+     * @return index (0..63) of least significant one bit
+     */
+    constexpr Square bitScanForward(BitBoardState bitBoard)
+    {
+        constexpr BitBoardState debruijn64 = 0x03f79d71b4cb0a89;
+        //assert (bitBoard != 0);
+        return Square(bitScanForwardLookup[((bitBoard ^ (bitBoard - 1)) * debruijn64) >> 58]);
+    }
 }
