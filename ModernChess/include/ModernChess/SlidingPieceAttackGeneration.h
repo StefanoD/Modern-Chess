18edf013c3ab9968c13b2cbfc43561981e53ebe6
Stefano Di Martino <stefano.d@posteo.de>
2023-04-01 21:14:08 +0200

    Add bishopAttacksOnTheFly() and maskRookAttacks()

--- ModernChess/include/ModernChess/SlidingPieceAttackGeneration.h
+++ ModernChess/include/ModernChess/SlidingPieceAttackGeneration.h
@@ -1,88 +1,223 @@
 #pragma once
 
 #include "BitBoardConstants.h"
+#include "ModernChess/BitBoardOperations.h"
 #include "MoveGenerations.h"
 #include "Square.h"
 
-namespace ModernChess::AttackGeneration::SlidingPieces
+namespace ModernChess::AttackGeneration::SlidingPieces {
+
+    // mask bishop attacks
+    constexpr BitBoardState maskBishopAttacks(Square square)
     {
+        // result attacks bitboard
+        BitBoardState attacks = BoardState::empty;
 
-        // mask bishop attacks
-        constexpr BitBoardState maskBishopAttacks(Square square)
+        // init target rank & files
+        const int targetRank = square / 8;
+        int const targetFile = square % 8;
+
+        // init ranks and files
+        int rank;
+        int file;
+
+        // mask relevant bishop occupancy bits
+        for (rank = targetRank + 1, file = targetFile + 1; rank <= 6 && file <= 6; rank++, file++)
         {
-            // result attacks bitboard
-            BitBoardState attacks = BoardState::empty;
-
-            // init target rank & files
-            const int targetRank = square / 8;
-            int const targetFile = square % 8;
-
-            // init ranks and files
-            int rank;
-            int file;
-
-            // mask relevant bishop occupancy bits
-            for (rank = targetRank + 1, file = targetFile + 1; rank <= 6 && file <= 6; rank++, file++)
-            {
-                attacks |= (1ULL << (rank * 8 + file));
-            }
-
-            for (rank = targetRank - 1, file = targetFile + 1; rank >= 1 && file <= 6; rank--, file++)
-            {
-                attacks |= (1ULL << (rank * 8 + file));
-            }
-
-            for (rank = targetRank + 1, file = targetFile - 1; rank <= 6 && file >= 1; rank++, file--)
-            {
-                attacks |= (1ULL << (rank * 8 + file));
-            }
-
-            for (rank = targetRank - 1, file = targetFile - 1; rank >= 1 && file >= 1; rank--, file--)
-            {
-                attacks |= (1ULL << (rank * 8 + file));
-            }
-
-            // return attack map
-            return attacks;
+            const Square attackedSquare = BitBoardOperations::getSquare(rank, file);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
         }
 
-        // mask rook attacks
-        constexpr BitBoardState maskRookAttacks(Square square)
+        for (rank = targetRank - 1, file = targetFile + 1; rank >= 1 && file <= 6; rank--, file++)
         {
-            // result attacks bitboard
-            BitBoardState attacks = BoardState::empty;
-
-            // init target rank & files
-            const int targetRank = square / 8;
-            const int targetFile = square % 8;
-
-            // init ranks & files
-            int rank, file;
-
-            // mask relevant bishop occupancy bits
-            for (rank = targetRank + 1; rank <= 6; rank++)
-            {
-                attacks |= (1ULL << (rank * 8 + targetFile));
-            }
-
-            for (rank = targetRank - 1; rank >= 1; rank--)
-            {
-                attacks |= (1ULL << (rank * 8 + targetFile));
-            }
-
-            for (file = targetFile + 1; file <= 6; file++)
-            {
-                attacks |= (1ULL << (targetRank * 8 + file));
-            }
-
-            for (file = targetFile - 1; file >= 1; file--)
-            {
-                attacks |= (1ULL << (targetRank * 8 + file));
-            }
-
-            // return attack map
-            return attacks;
+            const Square attackedSquare = BitBoardOperations::getSquare(rank, file);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
         }
+
+        for (rank = targetRank + 1, file = targetFile - 1; rank <= 6 && file >= 1; rank++, file--)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(rank, file);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+        }
+
+        for (rank = targetRank - 1, file = targetFile - 1; rank >= 1 && file >= 1; rank--, file--)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(rank, file);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+        }
+
+        // return attack map
+        return attacks;
+    }
+
+    // generate bishop attacks on the fly
+    constexpr BitBoardState bishopAttacksOnTheFly(Square square, BitBoardState occupiedSquares)
+    {
+        // result attacks bitboard
+        BitBoardState attacks = BoardState::empty;
+
+        // init target rank & files
+        const int targetRank = square / 8;
+        const int targetFile = square % 8;
+
+        // init ranks & files
+        int rank;
+        int file;
+
+        // generate bishop atacks
+        for (rank = targetRank + 1, file = targetFile + 1; rank <= 7 && file <= 7; rank++, file++)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(rank, file);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+
+            if (BitBoardOperations::isOccupied(occupiedSquares, attackedSquare))
+            {
+                break;
+            }
+        }
+
+        for (rank = targetRank - 1, file = targetFile + 1; rank >= 0 && file <= 7; rank--, file++)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(rank, file);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+
+            if (BitBoardOperations::isOccupied(occupiedSquares, attackedSquare))
+            {
+                break;
+            }
+        }
+
+        for (rank = targetRank + 1, file = targetFile - 1; rank <= 7 && file >= 0; rank++, file--)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(rank, file);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+
+            if (BitBoardOperations::isOccupied(occupiedSquares, attackedSquare))
+            {
+                break;
+            }
+        }
+
+        for (rank = targetRank - 1, file = targetFile - 1; rank >= 0 && file >= 0; rank--, file--)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(rank, file);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+
+            if (BitBoardOperations::isOccupied(occupiedSquares, attackedSquare))
+            {
+                break;
+            }
+        }
+
+        // return attack map
+        return attacks;
+    }
+
+    // mask rook attacks
+    constexpr BitBoardState maskRookAttacks(Square square)
+    {
+        // result attacks bitboard
+        BitBoardState attacks = BoardState::empty;
+
+        // init target rank & files
+        const int targetRank = square / 8;
+        const int targetFile = square % 8;
+
+        // init ranks & files
+        int rank;
+        int file;
+
+        // mask relevant bishop occupancy bits
+        for (rank = targetRank + 1; rank <= 6; rank++)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(rank, targetFile);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+        }
+
+        for (rank = targetRank - 1; rank >= 1; rank--)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(rank, targetFile);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+        }
+
+        for (file = targetFile + 1; file <= 6; file++)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(targetRank, file);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+        }
+
+        for (file = targetFile - 1; file >= 1; file--)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(targetRank, file);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+        }
+
+        // return attack map
+        return attacks;
+    }
+
+    // generate rook attacks on the fly
+    constexpr BitBoardState rookAttacksOnTheFly(Square square, BitBoardState occupiedSquares)
+    {
+        // result attacks bitboard
+        BitBoardState attacks = BoardState::empty;
+
+        // init target rank & files
+        const int targetRank = square / 8;
+        const int targetFile = square % 8;
+
+        // init ranks & files
+        int rank;
+        int file;
+
+        // generate rook attacks
+        for (rank = targetRank + 1; rank <= 7; rank++)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(rank, targetFile);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+
+            if (BitBoardOperations::isOccupied(occupiedSquares, attackedSquare))
+            {
+                break;
+            }
+        }
+
+        for (rank = targetRank - 1; rank >= 0; rank--)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(rank, targetFile);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+
+            if (BitBoardOperations::isOccupied(occupiedSquares, attackedSquare))
+            {
+                break;
+            }
+        }
+
+        for (file = targetFile + 1; file <= 7; file++)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(targetRank, file);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+
+            if (BitBoardOperations::isOccupied(occupiedSquares, attackedSquare))
+            {
+                break;
+            }
+        }
+
+        for (file = targetFile - 1; file >= 0; file--)
+        {
+            const Square attackedSquare = BitBoardOperations::getSquare(targetRank, file);
+            attacks = BitBoardOperations::occupySquare(attacks, attackedSquare);
+
+            if (BitBoardOperations::isOccupied(occupiedSquares, attackedSquare))
+            {
+                break;
+            }
+        }
+
+        // return attack map
+        return attacks;
+    }
 
 
         /*BitBoardState attack_table[...]; // ~840 KiB all rook and bishop attacks, less with constructive collisions optimization
