542be50410d7bace94744c4162c5c9c780f3065c
Stefano Di Martino <stefano.d@posteo.de>
2023-04-01 16:50:04 +0200

    Add test maskRookAttacksD4

--- ModernChess/test/SlidingPieceAttackGenerationTest.cpp
+++ ModernChess/test/SlidingPieceAttackGenerationTest.cpp
@@ -43,4 +43,42 @@ namespace {
 
         print(std::cout, board) << std::endl;
     }
+
+    TEST(SlidingPieceAttackGeneration, maskRookAttacksD4)
+    {
+        const BitBoardState board = AttackGeneration::SlidingPieces::maskRookAttacks(d4);;
+
+
+        const std::vector<Square> expectedOccupiedSquares{
+                            d2,
+                            d3,
+                    b4, c4,     e4, f4, g4,
+                            d5,
+                            d6,
+                            d7,
+        };
+
+        const std::vector<Square> expectedEmptySquares{
+                a1, b1, c1, d1, e1, f1, g1, h1,
+                a2, b2, c2,     e2, f2, g2, h2,
+                a3, b3, c3,     e3, f3, g3, h3,
+                a2,         d4,             h4,
+                a5, b5, c5,     e5, f5, g5, h5,
+                a6, b6, c6,     e6, f6, g6, h6,
+                a7, b7, c7,     e7, f7, g7, h7,
+                a8, b8, c8, d8, e8, f8, g8, h8,
+        };
+
+        for (const Square square: expectedOccupiedSquares)
+        {
+            EXPECT_TRUE(BitBoardOperations::isOccupied(board, square));
+        }
+
+        for (const Square square: expectedEmptySquares)
+        {
+            EXPECT_FALSE(BitBoardOperations::isOccupied(board, square));
+        }
+
+        print(std::cout, board) << std::endl;
+    }
 }
