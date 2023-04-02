ab5aa4e89e04c039c7722e89a93256bd83431558
Stefano Di Martino <stefano.d@posteo.de>
2023-04-02 16:50:55 +0200

    Use int32_t

--- ModernChess/include/ModernChess/Square.h
+++ ModernChess/include/ModernChess/Square.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <array>
+#include <cinttypes>
 
 namespace ModernChess {
     // Constants from https://www.chessprogramming.org/Square_Mapping_Considerations
     // We use Little-Endian Rank-File Mapping
 
-    enum Square : int
+    enum Square : int32_t
     {
         a1, b1, c1, d1, e1, f1, g1, h1,
         a2, b2, c2, d2, e2, f2, g2, h2,
