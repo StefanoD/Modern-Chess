0348c554bec80c37fe579d7cc5d93755dabc0de8
Stefano Di Martino <stefano.d@posteo.de>
2023-04-02 16:08:30 +0200

    Add setOccupancy()

--- ModernChess/test/SlidingPieceAttackGenerationTest.cpp
+++ ModernChess/test/SlidingPieceAttackGenerationTest.cpp
@@ -1,6 +1,9 @@
 #include "ModernChess/SlidingPieceAttackGeneration.h"
+#include "ModernChess/BitBoardOperations.h"
+#include "ModernChess/Square.h"
 #include "ModernChess/Utilities.h"
 
+#include <_types/_uint32_t.h>
 #include <gtest/gtest.h>
 
 namespace {
@@ -11,22 +14,22 @@ namespace {
         const BitBoardState board = AttackGeneration::SlidingPieces::maskBishopAttacks(d4);
 
         const std::vector<Square> expectedOccupiedSquares{
-                    b2,             f2,
-                        c3,     e3,
+                b2, f2,
+                c3, e3,
                 //          d4
-                        c5,     e5,
-                    b6,             f6,
-                                        g7,
+                c5, e5,
+                b6, f6,
+                g7,
         };
 
         const std::vector<Square> expectedEmptySquares{
                 a1, b1, c1, d1, e1, f1, g1, h1,
-                a2,     c2, d2, e2,     g2, h2,
-                a3, b3,     d3,     f3, g3, h3,
+                a2, c2, d2, e2, g2, h2,
+                a3, b3, d3, f3, g3, h3,
                 a4, b4, c4, d4, e4, f4, g4, h4,
-                a5, b5,     d5,     f5, g5, h5,
-                a6,     c6, d6, e6,     g6, h6,
-                a7, b7, c7, d7, e7, f7,     h7,
+                a5, b5, d5, f5, g5, h5,
+                a6, c6, d6, e6, g6, h6,
+                a7, b7, c7, d7, e7, f7, h7,
                 a8, b8, c8, d8, e8, f8, g8, h8,
         };
 
@@ -53,13 +56,13 @@ namespace {
         };
 
         const std::vector<Square> expectedEmptySquares{
-                    b1, c1, d1, e1, f1,     h1,
-                a2,     c2, d2, e2,     g2, h2,
-                a3, b3,     d3,     f3, g3, h3,
+                b1, c1, d1, e1, f1, h1,
+                a2, c2, d2, e2, g2, h2,
+                a3, b3, d3, f3, g3, h3,
                 a4, b4, c4, d4, e4, f4, g4, h4,
-                a5, b5,     d5,     f5, g5, h5,
-                a6,     c6, d6, e6, f6, g6, h6,
-                    b7, c7, d7, e7, f7, g7, h7,
+                a5, b5, d5, f5, g5, h5,
+                a6, c6, d6, e6, f6, g6, h6,
+                b7, c7, d7, e7, f7, g7, h7,
                 a8, b8, c8, d8, e8, f8, g8, h8,
         };
 
@@ -81,22 +84,22 @@ namespace {
         const BitBoardState board = AttackGeneration::SlidingPieces::maskRookAttacks(d4);
 
         const std::vector<Square> expectedOccupiedSquares{
-                            d2,
-                            d3,
-                    b4, c4,     e4, f4, g4,
-                            d5,
-                            d6,
-                            d7,
+                d2,
+                d3,
+                b4, c4, e4, f4, g4,
+                d5,
+                d6,
+                d7,
         };
 
         const std::vector<Square> expectedEmptySquares{
                 a1, b1, c1, d1, e1, f1, g1, h1,
-                a2, b2, c2,     e2, f2, g2, h2,
-                a3, b3, c3,     e3, f3, g3, h3,
-                a2,         d4,             h4,
-                a5, b5, c5,     e5, f5, g5, h5,
-                a6, b6, c6,     e6, f6, g6, h6,
-                a7, b7, c7,     e7, f7, g7, h7,
+                a2, b2, c2, e2, f2, g2, h2,
+                a3, b3, c3, e3, f3, g3, h3,
+                a2, d4, h4,
+                a5, b5, c5, e5, f5, g5, h5,
+                a6, b6, c6, e6, f6, g6, h6,
+                a7, b7, c7, e7, f7, g7, h7,
                 a8, b8, c8, d8, e8, f8, g8, h8,
         };
 
@@ -119,17 +122,58 @@ namespace {
         const BitBoardState board = AttackGeneration::SlidingPieces::rookAttacksOnTheFly(playBoard, d4);
 
         const std::vector<Square> expectedOccupiedSquares{
-            a4, b4, c4, d1, d2, d3, d5, d6, e4, f4, g4, h4,
+                a4, b4, c4, d1, d2, d3, d5, d6, e4, f4, g4, h4,
         };
 
         const std::vector<Square> expectedEmptySquares{
-                a1, b1, c1,     e1, f1, g1, h1,
-                a2, b2, c2,     e2, f2, g2, h2,
-                a3, b3, c3,     e3, f3, g3, h3,
-                            d4,
-                a5, b5, c5,     e5, f5, g5, h5,
-                a6, b6, c6,     e6, f6, g6, h6,
+                a1, b1, c1, e1, f1, g1, h1,
+                a2, b2, c2, e2, f2, g2, h2,
+                a3, b3, c3, e3, f3, g3, h3,
+                d4,
+                a5, b5, c5, e5, f5, g5, h5,
+                a6, b6, c6, e6, f6, g6, h6,
                 a7, b7, c7, d7, e7, f7, g7, h7,
+                a8, b8, c8, d8, e8, f8, g8, h8,
+        };
+
+        for (const Square square: expectedOccupiedSquares)
+        {
+            EXPECT_TRUE(BitBoardOperations::isOccupied(board, square));
+        }
+
+        for (const Square square: expectedEmptySquares)
+        {
+            EXPECT_FALSE(BitBoardOperations::isOccupied(board, square));
+        }
+
+        print(std::cout, board) << std::endl;
+    }
+
+    TEST(SlidingPieceAttackGeneration, setOccupancy)
+    {
+        const BitBoardState attackMask = AttackGeneration::SlidingPieces::maskRookAttacks(Square::a1);
+        const uint32_t numberBits = BitBoardOperations::countBits(attackMask);
+        const uint32_t magicNumber = 4095;
+        const BitBoardState board = AttackGeneration::SlidingPieces::setOccupancy(uint32_t, numberBits, attackMask);
+
+        const std::vector<Square> expectedOccupiedSquares{
+                   b1, c1, d1, e1, f1, g1,
+                a2,
+                a3,
+                a4,
+                a5,
+                a6,
+                a7,
+        };
+
+        const std::vector<Square> expectedEmptySquares{
+                a1,                         h1,
+                    b2, c2, d2, e2, f2, g2, h2,
+                    b3, c3, d3, e3, f3, g3, h3,
+                    b4, c4, d4, e4, f4, g4, h4,
+                    b5, c5, d5, e5, f5, g5, h5,
+                    b6, c6, d6, e6, f6, g6, h6,
+                    b7, c7, d7, e7, f7, g7, h7,
                 a8, b8, c8, d8, e8, f8, g8, h8,
         };
 
