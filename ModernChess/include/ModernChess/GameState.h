c7b3776180267c7a0bfedf2c588c695a10e5ea5f
Stefano Di Martino <stefano.d@posteo.de>
2023-04-09 01:27:26 +0200

    add CasteRights

--- ModernChess/include/ModernChess/GameState.h
+++ ModernChess/include/ModernChess/GameState.h
@@ -6,6 +6,28 @@
 
 namespace ModernChess
 {
+
+    /**
+       binary | decimal
+
+       0001     1  white king can castle to the king side
+       0010     2  white king can castle to the queen side
+       0100     4  black king can castle to the king side
+       1000     8  black king can castle to the queen side
+       examples
+       1111       both sides can castle both directions
+       1001       black king => queen side
+                  white king => king side
+    */
+
+    enum CasteRights
+    {
+        WhiteKingSide = 1,
+        WhiteQueenSide = 2,
+        BlackKindSide = 4,
+        BlackQueenSide = 8
+    };
+
     class GameState
     {
     public:
@@ -15,8 +37,7 @@ namespace ModernChess
         // See https://www.chessprogramming.org/Halfmove_Clock
         uint32_t m_halfMoveClock = 0;
         Color m_sideToMove = Color::White;
-        bool m_whiteHasCastleRights = true;
-        bool m_blackHasCastleRights = true;
+        CasteRights casteRights{};
         Square m_enPassantTarget = Square::undefined;
     };
 }
