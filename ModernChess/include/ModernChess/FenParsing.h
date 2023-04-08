84373dae42d98e36c480cece78451bcbe2987fb7
Stefano Di Martino <stefano.d@posteo.de>
2023-04-09 01:24:12 +0200

    add FEN string

--- ModernChess/include/ModernChess/FenParsing.h
+++ ModernChess/include/ModernChess/FenParsing.h
@@ -1,9 +1,14 @@
 #pragma once
 
-#pragma once
-
+#include <array>
+#include <string_view>
 
 namespace ModernChess {
+    // See https://de.wikipedia.org/wiki/Forsyth-Edwards-Notation
 
+    // ASCII pieces
+    constexpr auto asciiFigures = "PNBRQKpnbrqk";
 
+    // unicode pieces
+    constexpr std::array<std::string_view, 12> unicodeFigures {"♙", "♘", "♗", "♖", "♕", "♔", "♟︎", "♞", "♝", "♜", "♛", "♚"};
 }
