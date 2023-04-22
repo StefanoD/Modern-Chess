83866d12b4ab08f361ebe711d5ef339e753c9f63
Stefano Di Martino <stefano.d@posteo.de>
2023-04-22 20:53:28 +0200

    BlackPawnOnC5AttacksWhitePawnOnD4

--- ModernChess/test/AttackQueriesTest.cpp
+++ ModernChess/test/AttackQueriesTest.cpp
@@ -8,7 +8,7 @@ namespace {
     using namespace ModernChess;
     using namespace ModernChess::FenParsing;
 
-    TEST(AttackQueriesTest, whitePawnOnD4BlackPawnOnC5)
+    TEST(AttackQueriesTest, whitePawnOnD4AttacksBlackPawnOnC5)
     {
         /*
            8 . . . . . . . ♔
@@ -30,8 +30,43 @@ namespace {
 
         EXPECT_TRUE(ableToCaptureEast);
 
+        const bool ableToCaptureWest = AttackQueries::squareIsAttackedByWhite(gameState.board, Square::c5);
+
+        EXPECT_TRUE(ableToCaptureWest);
+
         std::cout << gameState;
 
         printAttackedSquares(std::cout, gameState.board, Color::White) << std::endl;
+    }
+
+    TEST(AttackQueriesTest, BlackPawnOnC5AttacksWhitePawnOnD4)
+    {
+        /*
+           8 . . . . . . . ♔
+           7 . . . . . . . .
+           6 . . . . . . . .
+           5 . . ♙ . . . . .
+           4 . . . ♟︎ . . . .
+           3 . . . . . . . .
+           2 . . . . . . . .
+           1 . . . . . . . ♚
+
+             a b c d e f g h
+         */
+        const auto fenString = "7k/8/8/2p5/3P4/8/8/7K w - - 0 1";
+        FenParser fenParser;
+        const GameState gameState = fenParser.parse(fenString);
+        // Occupy Black pawnAttackTable on c5
+        const bool ableToCaptureEast = AttackQueries::squareIsAttackedByBlack(gameState.board, Square::b4);
+
+        EXPECT_TRUE(ableToCaptureEast);
+
+        const bool ableToCaptureWest = AttackQueries::squareIsAttackedByBlack(gameState.board, Square::d4);
+
+        EXPECT_TRUE(ableToCaptureWest);
+
+        std::cout << gameState;
+
+        printAttackedSquares(std::cout, gameState.board, Color::Black) << std::endl;
     }
 }
