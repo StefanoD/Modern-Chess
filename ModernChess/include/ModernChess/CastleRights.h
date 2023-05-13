a5c9d2ca5b1248d254d1b39d1e829b38ba52e2a5
Stefano Di Martino <stefano.d@posteo.de>
2023-05-13 22:09:30 +0200

    Add method updateCastleRights()

--- ModernChess/include/ModernChess/CastleRights.h
+++ ModernChess/include/ModernChess/CastleRights.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Square.h"
+
 #include <ostream>
 
 namespace ModernChess
@@ -26,6 +28,35 @@ namespace ModernChess
         BlackQueenSide = 8,
         BlackAnySide = BlackKingSide + BlackQueenSide
     };
+
+    namespace Details
+    {
+        /*
+         * For updating castle right in an efficient way when a piece has moved
+         *                              castling    move     in      in
+         *                              right       update   binary  decimal
+         *   king & rooks didn't move:  1111    &   1111  =  1111    15
+         *          white king  moved:  1111    &   1100  =  1100    12
+         *    white king's rook moved:  1111    &   1110  =  1110    14
+         *   white queen's rook moved:  1111    &   1101  =  1101    13
+         *
+         *           black king moved:  1111    &   0011  =  1011    3
+         *    black king's rook moved:  1111    &   1011  =  1011    11
+         *   black queen's rook moved:  1111    &   0111  =  0111    7
+        */
+
+        // castling rights update constants
+        constexpr std::array<uint8_t, 64> castlingRightsLookUpTable {
+                13, 15, 15, 15, 12, 15, 15, 14,
+                15, 15, 15, 15, 15, 15, 15, 15,
+                15, 15, 15, 15, 15, 15, 15, 15,
+                15, 15, 15, 15, 15, 15, 15, 15,
+                15, 15, 15, 15, 15, 15, 15, 15,
+                15, 15, 15, 15, 15, 15, 15, 15,
+                15, 15, 15, 15, 15, 15, 15, 15,
+                7, 15, 15, 15,  3, 15, 15, 11
+        };
+    }
 
     constexpr bool whiteCanCastleKingSide(CastleRights castleRights)
     {
@@ -65,6 +96,13 @@ namespace ModernChess
     constexpr CastleRights addBlackQueenSideCastleRights(CastleRights castleRights)
     {
         return CastleRights(castleRights | CastleRights::BlackQueenSide);
+    }
+
+    constexpr CastleRights updateCastleRights(CastleRights castleRights, Square movedFrom, Square movedTo)
+    {
+        return CastleRights(castleRights &
+                            Details::castlingRightsLookUpTable[movedFrom] &
+                            Details::castlingRightsLookUpTable[movedTo]);
     }
 }
 
