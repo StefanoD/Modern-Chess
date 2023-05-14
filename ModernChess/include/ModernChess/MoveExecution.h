b1c4518c7e67ab555bcc53319a697b2bb59d459c
Stefano Di Martino <stefano.d@posteo.de>
2023-05-14 16:24:44 +0200

    Add test EnPassantCaptureWithWhiteTest

---
+++ ModernChess/include/ModernChess/MoveExecution.h
@@ -0,0 +1,149 @@
+#pragma once
+
+#include "GameState.h"
+#include "BitBoardOperations.h"
+#include "AttackQueries.h"
+
+namespace ModernChess::MoveGenerations
+{
+    enum class MoveType
+    {
+        AllMoves,
+        CapturesOnly
+    };
+
+    class MoveExecution
+    {
+    public:
+        MoveExecution() = delete;
+
+        static bool executeMoveForWhite(GameState &gameState, Move move, MoveType moveType)
+        {
+            // quiet moves
+            if (moveType == MoveType::AllMoves)
+            {
+                // preserve board state
+                const Board boardCopy = gameState.board;
+
+                // parse move
+                const Square sourceSquare = move.getFrom();
+                const Square targetSquare = move.getTo();
+                const Figure movedFigure = move.getMovedFigure();
+                const bool castling = move.isCastlingMove();
+
+                // move movedFigure
+                removeFromBitboards(gameState, movedFigure, Color::White, sourceSquare);
+                addToBitboards(gameState, movedFigure, Color::White, targetSquare);
+
+                // handling capture moves
+                if (move.isCapture())
+                {
+                    // loop over bitboards opposite to the current side to move
+                    for (Figure figure = Figure::BlackPawn; figure <= Figure::BlackKing; ++figure)
+                    {
+                        // if there's a figure on the target square
+                        if (BitBoardOperations::isOccupied(gameState.board.bitboards[figure], targetSquare))
+                        {
+                            // remove it from corresponding bitboard
+                            removeFromBitboards(gameState, figure, Color::White, targetSquare);
+                            break;
+                        }
+                    }
+                }
+
+                // handle pawn promotions
+                if (const Figure promotedPiece = move.getPromotedPiece();
+                        promotedPiece != Figure::None)
+                {
+                    // erase the pawn from the target square
+                    removeFromBitboards(gameState, Figure::WhitePawn, Color::White, targetSquare);
+                    // set up promoted movedFigure on chess board
+                    addToBitboards(gameState, promotedPiece, Color::White, targetSquare);
+                }
+
+                // handle en passant captures
+                if (move.isEnPassantCapture())
+                {
+                    // The en passant capture is always one behind a double pawn push from the opponent
+                    const Square actualPawnSquare = BitBoardOperations::getSouthSquareFromGivenSquare(targetSquare);
+                    removeFromBitboards(gameState, Figure::BlackPawn, Color::Black, actualPawnSquare);
+                }
+
+                // reset en passant square
+                gameState.board.enPassantTarget = Square::undefined;
+
+                // handle double pawn push
+                if (move.isDoublePawnPush())
+                {
+                    gameState.board.enPassantTarget = BitBoardOperations::getSouthSquareFromGivenSquare(targetSquare);
+                }
+
+                // handle castling moves
+                if (castling)
+                {
+                    // switch target square
+                    switch (targetSquare)
+                    {
+                        case (Square::g1):
+                            // white castles king side --> move H rook
+                            removeFromBitboards(gameState, Figure::WhiteRook, Color::White, Square::h1);
+                            addToBitboards(gameState, Figure::WhiteRook, Color::White, Square::f1);
+                            break;
+
+                        case (Square::c1):
+                            // white castles queen side --> move A rook
+                            removeFromBitboards(gameState, Figure::WhiteRook, Color::White, Square::a1);
+                            addToBitboards(gameState, Figure::WhiteRook, Color::White, Square::d1);
+                            break;
+                        default:
+                            // TODO throw exception
+                            break;
+                    }
+                }
+
+                // update castling rights
+                gameState.board.castlingRights = updateCastlingRights(gameState.board.castlingRights, sourceSquare, targetSquare);
+                // change side to move
+                gameState.board.sideToMove = Color(!bool(gameState.board.sideToMove));
+
+                // make sure that king has not been exposed into a check
+                if (const Square kingsSquare = BitBoardOperations::bitScanForward(gameState.board.bitboards[Figure::WhiteKing]);
+                        AttackQueries::squareIsAttackedByBlack(gameState.board, kingsSquare))
+                {
+                    // take move back
+                    gameState.board = boardCopy;
+
+                    // return illegal move
+                    return false;
+                }
+                // return legal move
+                return true;
+            }
+
+            // capture moves
+            // make sure move is the capture
+            if (move.isCapture())
+            {
+                return executeMoveForWhite(gameState, move, MoveType::AllMoves);
+            }
+
+            // otherwise the move is not a capture
+            // don't make it
+            return false;
+        }
+    private:
+        static void removeFromBitboards(GameState &gameState, Figure figure, Color color, Square square)
+        {
+            gameState.board.bitboards[figure] = BitBoardOperations::eraseSquare(gameState.board.bitboards[figure], square);
+            gameState.board.occupancies[color] = BitBoardOperations::eraseSquare(gameState.board.occupancies[color], square);
+            gameState.board.occupancies[Color::Both] = BitBoardOperations::eraseSquare(gameState.board.occupancies[Color::Both], square);
+        }
+
+        static void addToBitboards(GameState &gameState, Figure figure, Color color, Square square)
+        {
+            gameState.board.bitboards[figure] = BitBoardOperations::occupySquare(gameState.board.bitboards[figure], square);
+            gameState.board.occupancies[color] = BitBoardOperations::occupySquare(gameState.board.occupancies[color], square);
+            gameState.board.occupancies[Color::Both] = BitBoardOperations::occupySquare(gameState.board.occupancies[Color::Both], square);
+        }
+    };
+}
---
+++ ModernChess/test/MoveExecutionTest.cpp
@@ -0,0 +1,49 @@
+#include "ModernChess/MoveExecution.h"
+#include "ModernChess/FenParsing.h"
+#include "ModernChess/Utilities.h"
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+
+using namespace ModernChess;
+using namespace ModernChess::MoveGenerations;
+
+namespace
+{
+    TEST(MoveExecutionTest, EnPassantCaptureWithWhiteTest)
+    {
+        /*
+         * 8 . . . . ♔ . . .
+         * 7 . . . . . . . .
+         * 6 . . . . . . . .
+         * 5 . . . . ♟︎ ♙ . .
+         * 4 . . . . . . . .
+         * 3 . . . . . . . .
+         * 2 . . . . . . . .
+         * 1 . . . . ♚ . . .
+         *
+         *   a b c d e f g h
+         */
+        constexpr auto enPassantPosition = "4k3/8/8/4Pp2/8/8/8/4K3 w - f6 0 1";
+        FenParsing::FenParser fenParser;
+        GameState gameState = fenParser.parse(enPassantPosition);
+
+        const Move move(Square::e5, Square::f6, Figure::WhitePawn, Figure::None, true, false, true, false);
+
+        std::cout << gameState << std::endl;
+
+        const bool success = MoveExecution::executeMoveForWhite(gameState, move, MoveType::AllMoves);
+
+        EXPECT_TRUE(success);
+        EXPECT_TRUE(BitBoardOperations::isOccupied(gameState.board.bitboards[Figure::WhitePawn], Square::f6));
+        EXPECT_TRUE(BitBoardOperations::isOccupied(gameState.board.occupancies[Color::White], Square::f6));
+        EXPECT_FALSE(BitBoardOperations::isOccupied(gameState.board.bitboards[Figure::WhitePawn], Square::e5));
+        EXPECT_FALSE(BitBoardOperations::isOccupied(gameState.board.bitboards[Figure::BlackPawn], Square::f6));
+        EXPECT_FALSE(BitBoardOperations::isOccupied(gameState.board.bitboards[Figure::BlackPawn], Square::f5));
+
+        std::cout << gameState << std::endl;
+    }
+
+
+}
