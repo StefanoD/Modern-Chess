711143be340f9b9c66ed42f192e3ea8291bfff2c
Stefano Di Martino <stefano.d@posteo.de>
2023-05-14 18:33:09 +0200

    Add test MoveExecutionTest::WhiteQueenSideCastle

--- ModernChess/test/MoveExecutionTest.cpp
+++ ModernChess/test/MoveExecutionTest.cpp
@@ -48,7 +48,7 @@ namespace
         std::cout << gameState << std::endl;
     }
 
-    TEST(MoveExecutionTest, WhiteKingAndQueenSideCastle)
+    TEST(MoveExecutionTest, WhiteKingSideCastle)
     {
         /*
          * 8 . . . . ♔ . . .
@@ -80,6 +80,45 @@ namespace
         EXPECT_TRUE(BitBoardOperations::isOccupied(gameState.board.occupancies[Color::White], Square::f1));
         EXPECT_FALSE(BitBoardOperations::isOccupied(gameState.board.bitboards[Figure::WhiteKing], Square::e1));
         EXPECT_FALSE(BitBoardOperations::isOccupied(gameState.board.bitboards[Figure::WhiteRook], Square::h1));
+        EXPECT_EQ(gameState.board.castlingRights, CastlingRights::NotPossible);
+        EXPECT_EQ(gameState.board.sideToMove, Color::Black);
+
+        std::cout << "After Move:" << std::endl;
+        std::cout << gameState << std::endl;
+    }
+
+    TEST(MoveExecutionTest, WhiteQueenSideCastle)
+    {
+        /*
+         * 8 . . . . ♔ . . .
+         * 7 . . . . . . . .
+         * 6 . . . . . . . .
+         * 5 . . . . . . . .
+         * 4 . . . . ♖ . . .
+         * 3 . . . . ♟︎ . . .
+         * 2 . . . . . . . .
+         * 1 ♜ . . . ♚ . . ♜
+         *
+         *   a b c d e f g h
+         */
+        constexpr auto kingSideCastlingPosition = "4k3/8/8/8/4r3/4P3/8/R3K2R w KQ - 0 1";
+        FenParsing::FenParser fenParser;
+        GameState gameState = fenParser.parse(kingSideCastlingPosition);
+
+        const Move move(Square::e1, Square::c1, Figure::WhiteKing, Figure::None, false, false, false, true);
+
+        std::cout << "Before Move:" << std::endl;
+        std::cout << gameState << std::endl << std::endl;
+
+        const bool success = MoveExecution::executeMoveForWhite(gameState, move, MoveType::AllMoves);
+
+        EXPECT_TRUE(success);
+        EXPECT_TRUE(BitBoardOperations::isOccupied(gameState.board.bitboards[Figure::WhiteKing], Square::c1));
+        EXPECT_TRUE(BitBoardOperations::isOccupied(gameState.board.bitboards[Figure::WhiteRook], Square::d1));
+        EXPECT_TRUE(BitBoardOperations::isOccupied(gameState.board.occupancies[Color::White], Square::c1));
+        EXPECT_TRUE(BitBoardOperations::isOccupied(gameState.board.occupancies[Color::White], Square::d1));
+        EXPECT_FALSE(BitBoardOperations::isOccupied(gameState.board.bitboards[Figure::WhiteKing], Square::e1));
+        EXPECT_FALSE(BitBoardOperations::isOccupied(gameState.board.bitboards[Figure::WhiteRook], Square::a1));
         EXPECT_EQ(gameState.board.castlingRights, CastlingRights::NotPossible);
         EXPECT_EQ(gameState.board.sideToMove, Color::Black);
 
