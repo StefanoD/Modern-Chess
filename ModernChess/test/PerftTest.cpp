48d919ca957832ae7b3853f1c09bf69c4a24dc0c
Stefano Di Martino <stefano.d@posteo.de>
2023-05-17 21:53:44 +0200

    Add more PerfTests

--- ModernChess/test/PerftTest.cpp
+++ ModernChess/test/PerftTest.cpp
@@ -16,4 +16,48 @@ namespace
 
         EXPECT_EQ(numberNodes, 193690690);
     }
+
+    TEST(PerftTest, TestPosition3Depth1To7)
+    {
+        PerformanceTest test(TestingPositions::Position3);
+
+        EXPECT_EQ(test.executePerformanceTest(4), 43238);
+        EXPECT_EQ(test.executePerformanceTest(5), 674624);
+        EXPECT_EQ(test.executePerformanceTest(6), 11030083);
+        EXPECT_EQ(test.executePerformanceTest(7), 178633661);
+    }
+
+    TEST(PerftTest, TestPosition4Depth1To6)
+    {
+        PerformanceTest test(TestingPositions::Position4);
+
+        EXPECT_EQ(test.executePerformanceTest(1), 6);
+        EXPECT_EQ(test.executePerformanceTest(2), 264);
+        EXPECT_EQ(test.executePerformanceTest(3), 9467);
+        EXPECT_EQ(test.executePerformanceTest(4), 422333);
+        EXPECT_EQ(test.executePerformanceTest(5), 15833292);
+        EXPECT_EQ(test.executePerformanceTest(6), 706045033);
+    }
+
+    TEST(PerftTest, TestPosition5Depth1To5)
+    {
+        PerformanceTest test(TestingPositions::Position5);
+
+        EXPECT_EQ(test.executePerformanceTest(1), 44);
+        EXPECT_EQ(test.executePerformanceTest(2), 1486);
+        EXPECT_EQ(test.executePerformanceTest(3), 62379);
+        EXPECT_EQ(test.executePerformanceTest(4), 2103487);
+        EXPECT_EQ(test.executePerformanceTest(5), 89941194);
+    }
+
+    TEST(PerftTest, TestPosition6Depth1To5)
+    {
+        PerformanceTest test(TestingPositions::Position6);
+
+        EXPECT_EQ(test.executePerformanceTest(1), 46);
+        EXPECT_EQ(test.executePerformanceTest(2), 2079);
+        EXPECT_EQ(test.executePerformanceTest(3), 89890);
+        EXPECT_EQ(test.executePerformanceTest(4), 3894594);
+        EXPECT_EQ(test.executePerformanceTest(5), 164075551);
+    }
 }
