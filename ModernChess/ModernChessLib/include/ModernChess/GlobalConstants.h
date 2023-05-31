00a972bdb831bc0b6f3f4b6f4775f6535bd35032
Stefano Di Martino <stefano.d@posteo.de>
2023-05-31 20:35:26 +0200

    Add global constants

---
+++ ModernChess/ModernChessLib/include/ModernChess/GlobalConstants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstddef>
+
+namespace ModernChess
+{
+    static constexpr size_t MaxHalfMoves = 256; ///< Or max number of ply
+    static constexpr size_t NumberOfFigureTypes = 12;
+    static constexpr size_t NumberOfSquares = 64;
+}
