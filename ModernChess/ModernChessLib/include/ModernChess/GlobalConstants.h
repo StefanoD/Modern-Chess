597dc5538db25a2afd1c2f144ed85d43e6dcb69b
Stefano Di Martino <stefano.d@posteo.de>
2023-06-03 19:19:37 +0200

    R>educe max number of half moves to 64

--- ModernChess/ModernChessLib/include/ModernChess/GlobalConstants.h
+++ ModernChess/ModernChessLib/include/ModernChess/GlobalConstants.h
@@ -4,7 +4,7 @@
 
 namespace ModernChess
 {
-    static constexpr size_t MaxHalfMoves = 256; ///< Or max number of ply
+    static constexpr size_t MaxHalfMoves = 64; ///< Or max number of ply
     static constexpr size_t NumberOfFigureTypes = 12;
     static constexpr size_t NumberOfSquares = 64;
 }
