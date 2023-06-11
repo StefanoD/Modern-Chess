6ad8c36910b157af63d552621e9d948ad2e8d2cc
Stefano Di Martino <stefano.d@posteo.de>
2023-06-11 11:14:08 +0200

    Add ZobristHasher.h

---
+++ ModernChess/ModernChessLib/include/ModernChess/ZobristHasher.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "GlobalConstants.h"
+
+#include <array>
+
+namespace ModernChess {
+    class ZobristHasher {
+    public:
+        // random piece keys [piece][square]
+        std::array<std::array<uint64_t, NumberOfFigureTypes>, NumberOfSquares> piece_keys{};
+
+        // random en passant keys [square]
+        std::array<uint64_t, NumberOfSquares> enpassant_keys;
+
+        // random castling keys
+        std::array<uint64_t, 16> castle_keys;
+
+        // random side key
+        uint64_t side_key;
+    };
+}
